e0f2ed9130593ed1884177f7b14121866d591deb
Sohaib Talaat Bhatti <sohaibbbhatti@gmail.com>
2014-06-22 05:42:05 +0500

    add functionality intent and style guideline

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,15 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functionality for creating a wrapper around matrices for computing and
+## caching the matrices inverse
+## Style guide: https://google-styleguide.googlecode.com/svn/trunk/Rguide.xml
+
 
 ## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
 ## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
 }
